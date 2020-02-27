a63a4d799891d6483eb75f79d0d8b76a296b4f49
Parker Hodge <parkerhodge@Parkers-MacBook-Pro.local>
2020-02-27 13:20:43 -0600

    something

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,8 @@
 import initializeDetailButtonEvents from './fish/dialog.js'
-import { useFish } from './fish/FishDataProvider.js'
 import FishList from './fish/Fishlist.js'
 import TipList from './tips/TipList.js'
 
-const arrayOfTheFish = useFish()
-
 FishList()
+TipList()
 
 initializeDetailButtonEvents()
-
-TipList()
